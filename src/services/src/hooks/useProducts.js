8faa99cb2223999525ae44c5980a333a65a0c792
Ayushi Rani <ayushirani0210@gmail.com>
2026-01-28 22:26:47 +0530

    Add custom hook for fetching products

---
+++ src/services/src/hooks/useProducts.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+import { fetchProducts } from "../services/api";
+
+
+export default function useProducts() {
+const [products, setProducts] = useState([]);
+const [loading, setLoading] = useState(true);
+
+
+useEffect(() => {
+fetchProducts().then((data) => {
+setProducts(data);
+setLoading(false);
+});
+}, []);
+
+
+return { products, loading };
+}
